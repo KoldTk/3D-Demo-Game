c572f01ba865f468334cff99f85dc31161709d05
Koldtk <koldtk@gmail.com>
2025-03-04 16:27:20 +0700

    Demo 2 - Done

--- Assets/ShootingMechanic.cs
+++ Assets/ShootingMechanic.cs
@@ -5,18 +5,30 @@ using UnityEngine;
 
 public class ShootingMechanic : MonoBehaviour
 {
+    private const int LeftMouseButton = 0;
     public GameObject bulletPrefab;
     public Transform firingPos;
     public float bulletSpeed;
     public AudioSource shootingSound;
+    public Animator anim;
 
     // Start is called before the first frame update
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(LeftMouseButton))
         {
-            
+            ShootBullet();
+            PlayFireSound();
+            AddProjectile();
         }
     }
+
+    private void ShootBullet() => anim.SetTrigger("Shoot");
+    public void PlayFireSound() => shootingSound.Play();
+    public void AddProjectile()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firingPos.position, firingPos.rotation);
+        bullet.GetComponent<Rigidbody>().velocity = firingPos.forward * bulletSpeed;
+    }    
 
 }
